59d49fa1be11692f4f60838d4f318e7aa81f46fc
shubham dahiya <dahiya.shubham1234@gmail.com>
2024-01-10 10:43:45 +0530

    first commit

---
+++ routes/profile.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const router = express.Router()
+const { auth, isAdmin } = require("../middleware/auth")
+const {
+  deleteAccount,
+  updateProfile,
+  getAllUserDetails,
+  updateDisplayPicture
+} = require("../controllers/profile")
+
+// ********************************************************************************************************
+//                                      Profile routes
+// ********************************************************************************************************
+// Delete User Account
+router.delete("/deleteProfile", auth, deleteAccount)
+router.put("/updateProfile", auth, updateProfile)
+router.get("/getUserDetails", auth, getAllUserDetails)
+router.put("/updateDisplayPicture", auth, updateDisplayPicture)
+
+module.exports = router
